e6ef587f0d4cb740f06d5b05ed951683b6639b38
Diya Arora <diya05arora@gmail.com>
2024-06-21 10:44:54 +0530

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,16 @@
+# Every variable is an object of a particular class in Python
+# For example
+l = [1,2,3,4,5]
+# l is object of class 'list'
+print(type(l))
+
+a = 10
+# a is object of class 'int'
+print(type(a))
+
+# PROCEDURAL ORIENTED PROGRAMMING LANGUAGE
+# creates a step by step program that guides the application through a sequence of instructions. 
+# Each instruction is executed in order.
+
+# OBJECT ORIENTED PROGRAMMING LANGUAGE
+# each program is made up of many entities called objects.
